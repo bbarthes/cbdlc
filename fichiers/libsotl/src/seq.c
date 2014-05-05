267d059f81f44e63900afdd670f8399cfde208d7
benoît barthés <benoit.barthes@etu.u-bordeaux1.fr>
2014-05-05 16:48:05 +0200

    fin probléme

--- fichiers/libsotl/src/seq.c
+++ fichiers/libsotl/src/seq.c
@@ -19,6 +19,7 @@ static int *atom_state = NULL;
 
 // Update OpenGL Vertex Buffer Object
 //
+
 static void seq_update_vbo (sotl_device_t *dev)
 {
   sotl_atom_set_t *set = &dev->atom_set;
@@ -38,7 +39,7 @@ static void seq_update_vbo (sotl_device_t *dev)
       vbo_color[n*3 + 0] = (1.0 - ratio) * atom_color[0].R + ratio * 1.0;
       vbo_color[n*3 + 1] = (1.0 - ratio) * atom_color[0].G + ratio * 0.5;
       vbo_color[n*3 + 2] = (1.0 - ratio) * atom_color[0].B + ratio * 0.0;
-      printf("%d\n",atom_state[n]);
+     // printf("%d\n",atom_state[n]);
       //atom_state[n]--;
     }
   }
@@ -198,6 +199,7 @@ static void resTab(int * tab , unsigned sizeofTab)
         tab[i]= 0;
     }
 }
+
 static void prefixTab(int * source , int * dest, unsigned size)
 {
     dest[0] = 0;
@@ -233,7 +235,7 @@ static void sortAtomBox( sotl_domain_t *dom , sotl_atom_set_t *set)
 
     prefixTab(boxSort->nbAtomToBox, boxSort->preNbAtomToBox, dom->total_boxes);
 	resTab(boxSort->nbAtomToBox, dom->total_boxes);
-	
+
 	for(int i = 0 ; i < n; i++)
 	{
         int pos =atom_get_num_box(dom,set->pos.x[i], set->pos.y[i], set->pos.z[i],BOX_SIZE_INV);
@@ -241,9 +243,11 @@ static void sortAtomBox( sotl_domain_t *dom , sotl_atom_set_t *set)
         boxSort->swapState[i] = pos;
 
 	}
+
 	switchPtr(set);
 
 }
+
 int get_num_box(const sotl_domain_t *dom, const int x, const int y,
                      const int z)
 {
@@ -259,8 +263,8 @@ int get_num_box(const sotl_domain_t *dom, const int x, const int y,
               box_y * dom->boxes[0] +
               box_x;
 
-    if(box_id >= 0 && (unsigned)box_id < dom->total_boxes)
-		return -1;
+    if(box_id < 0 && (unsigned)box_id < dom->total_boxes)
+        return -1;
 		
     return box_id;
 }
@@ -273,11 +277,14 @@ static void seq_force_cube (sotl_device_t *dev)
 	
 	sortAtomBox(dom,set);
 	
+
+
 	
-	
-    /*
-	unsigned currentBox,otherBox;
-		
+    //*
+    unsigned currentBox;
+    int otherBox;
+    calc_t sq_dist;
+
 	for(int x = 0; x < dom->boxes[0]; x++)
         for(int y = 0; y < dom->boxes[1]; y++)
 			for(int z = 0; z < dom->boxes[2]; z++)
@@ -288,6 +295,7 @@ static void seq_force_cube (sotl_device_t *dev)
 					for(int yadj = -1; yadj < 2; yadj++)
 						for(int zadj = -1; zadj < 2; zadj++)
 						{
+
 							int xother = x +xadj;
 							int yother = y +yadj;
 							int zother = z +zadj;
@@ -295,15 +303,19 @@ static void seq_force_cube (sotl_device_t *dev)
                             if(otherBox > -1)
 							{
 
+                              //  printf("current %d , other %d  \n", currentBox,otherBox);
+
 								for (unsigned current = boxSort->preNbAtomToBox[currentBox]; current < boxSort->preNbAtomToBox[currentBox+1]; current++) 
 								{
+                                  //  printf("bite %d\n",current);
 									calc_t force[3] = { 0.0, 0.0, 0.0 };
 
 									for (unsigned other = boxSort->preNbAtomToBox[otherBox]; other < boxSort->preNbAtomToBox[otherBox+1]; other++)
 									{
+                                      //  printf("coucou %d\n",other);
 										if (current != other) 
 										{
-											calc_t sq_dist = squared_distance (set, current, other);
+                                            sq_dist = squared_distance (set, current, other);
 
 											if (sq_dist < LENNARD_SQUARED_CUTOFF) 
 											{
