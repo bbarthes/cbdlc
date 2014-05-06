0eb090bbf9cbb1e91c1225f369010784821b5885
benoît barthés <benoit.barthes@etu.u-bordeaux1.fr>
2014-05-06 17:19:45 +0200

    simplification

--- fichiers/libsotl/src/seq.c
+++ fichiers/libsotl/src/seq.c
@@ -4,6 +4,7 @@
 #include "device.h"
 #include "seq.h"
 #include "sotl.h"
+#include <assert.h>
 
 #ifdef HAVE_LIBGL
 #include "vbo.h"
@@ -91,21 +92,22 @@ static void seq_bounce (sotl_device_t *dev)
   for (unsigned n = 0; n < set->natoms; n++) {
   	for(int i = 0; i <3 ; i++)
   	{
-  		if(set->pos.x[n+set->offset*i]<= domain->min_ext[i])
+        if(set->pos.x[n+set->offset*i]<= domain->min_ext[i])
   		{
   			//TODO pour éviter d'avoir des atome qui ne sont pas dans des boite on force les cordonné au plus/moins = max_ext/min_ext
-  			set->pos.x[n+set->offset*i] = domain->min_ext[i];
+            //set->pos.x[n+set->offset*i] = domain->min_ext[i]+ BOX_SIZE_INV/2;
   			
-  			if(set->speed.dx[n+set->offset*i] < 0)
+            if(set->speed.dx[n+set->offset*i] < 0)
   			{set->speed.dx[n+set->offset*i]*= -1;
             //atom_state[n] = SHOCK_PERIOD;
   			}
   		}
   		if(set->pos.x[n+set->offset*i] >= domain->max_ext[i])
   		{
-  			set->pos.x[n+set->offset*i] = domain->max_ext[i];
+            //set->pos.x[n+set->offset*i] = domain->max_ext[i]- BOX_SIZE_INV/2;
   		
-  			if(set->speed.dx[n+set->offset*i] > 0){
+            if(set->speed.dx[n+set->offset*i] > 0)
+            {
 				set->speed.dx[n+set->offset*i]*= -1;
                 //atom_state[n] = SHOCK_PERIOD;
   			}
@@ -171,7 +173,7 @@ static void seq_force_old (sotl_device_t *dev)
     set->speed.dx[set->offset * 2 + current] += force[2];
   }
 }
-
+//fonction de teste plus utilse
 static void copiePtr( sotl_atom_set_t *set)
 {
 
@@ -232,13 +234,15 @@ static void moveAtomBox(int nAtom, int numbox, sotl_atom_set_t *set)
 
     int newPos = boxSort->preNbAtomToBox[numbox] + boxSort->nbAtomToBox[numbox];
 
+    assert(numbox >= 0);
+
     for(unsigned j = 0 ; j < 3 ; j++)
     {
         int offset = set->offset * j;
         boxSort->swapPosx[newPos + offset] = set->pos.x[nAtom + offset];
         boxSort->swapSpeedx[newPos + offset] = set->speed.dx[nAtom +offset];
     }
-    boxSort->swapState[newPos] = atom_state[nAtom];
+    //boxSort->swapState[newPos] = atom_state[nAtom];
     boxSort->nbAtomToBox[numbox]++;
 }
 
@@ -262,8 +266,8 @@ static void sortAtomBox( sotl_domain_t *dom , sotl_atom_set_t *set)
 
 	}
 
-    //switchPtr(set);
-    copiePtr(set);
+    switchPtr(set);
+    //copiePtr(set);
 
 }
 
@@ -292,21 +296,83 @@ int get_num_box(const sotl_domain_t *dom, const int x, const int y, const int z)
     return box_id;
 }
 
+void get_pos_box(const sotl_domain_t *dom, int nbBox, unsigned* pos)
+{
+    if(nbBox < 0 || pos == NULL){
+        //pos = NULL;
+        return;
+    }
+
+    int oY = dom->boxes[0];
+    int oZ = oY * dom->boxes[1];
+
+    int box_x, box_y, box_z;
+    int rrc  = BOX_SIZE_INV;
+
+    box_z =  nbBox/oZ;
+    box_x =  nbBox%oZ;
+    box_y =  box_x/oY;
+    box_x =  box_x%oY;
+
+    pos[0] = box_x/rrc;
+    pos[1] = box_y/rrc;
+    pos[2] = box_z/rrc;
+}
+
+
+static void computeForce(sotl_atom_set_t *set, int currentBox, int otherBox){
+    //  printf("current %d , other %d  \n", currentBox,otherBox);
+    calc_t sq_dist;
+      for (int current = boxSort->preNbAtomToBox[currentBox];
+      current < boxSort->preNbAtomToBox[currentBox+1];
+      current++)
+      {
+
+          calc_t force[3] = { 0.0, 0.0, 0.0 };
+
+          for (int other = boxSort->preNbAtomToBox[otherBox];
+           other < boxSort->preNbAtomToBox[otherBox+1];
+           other++)
+          {
+            //  printf("coucou %d\n",other);
+              if (current != other ) //&& current != NULL && other != NULL)//current other pas négatif car unsigned
+              {
+                  assert(current >= 0);
+                  assert(other >= 0);
+                  sq_dist = squared_distance (set, current, other);
+
+                  if (sq_dist < LENNARD_SQUARED_CUTOFF)
+                  {
+                    calc_t intensity = lennard_jones (sq_dist);
+
+                    force[0] += intensity * (set->pos.x[current] - set->pos.x[other]);
+                    force[1] += intensity * (set->pos.x[set->offset + current] -
+                                 set->pos.x[set->offset + other]);
+                    force[2] += intensity * (set->pos.x[set->offset * 2 + current] -
+                                 set->pos.x[set->offset * 2 + other]);
+                  }
+
+              }
+          }
+          set->speed.dx[current] += force[0];
+          set->speed.dx[set->offset + current] += force[1];
+          set->speed.dx[set->offset * 2 + current] += force[2];
+      }
+
+}
+
 
 static void seq_force_cube (sotl_device_t *dev)
 {
 	sotl_atom_set_t *set = &dev->atom_set;
 	sotl_domain_t *dom = &dev->domain;
 	
-	sortAtomBox(dom,set);
-	
-
-
+    sortAtomBox(dom,set);
 	
     //*
-    unsigned currentBox;
+    int currentBox;
     int otherBox;
-    calc_t sq_dist;
+    //calc_t sq_dist;
 /*	for(unsigned i = 0 ; i < dom->total_boxes+1; i++)
 	{
 		if(boxSort->preNbAtomToBox[i] < 0)
@@ -314,14 +380,30 @@ static void seq_force_cube (sotl_device_t *dev)
 			printf("i = %d num = %d\n", i, boxSort->preNbAtomToBox[i]);
 		}
 	}*/
-			
-	for(unsigned x = 0; x < dom->boxes[0]; x++)
+
+//*    //TODO optimisation invertion des boucles pour le cache
+    for(unsigned z = 0; z < dom->boxes[2]; z++)
         for(unsigned y = 0; y < dom->boxes[1]; y++)
-			for(unsigned z = 0; z < dom->boxes[2]; z++)
+            for(unsigned x = 0; x < dom->boxes[0]; x++)
+
+
 			{
                 currentBox = get_num_box(dom,x,y,z);
+
+                // CHECK
+               // printf("CHECK BEGIN\n");
+                unsigned pos[3] ={0,0,0};
+                get_pos_box(dom, currentBox, pos);
+                if(x!=pos[0])   printf("fuck X %d,%d\n",x,pos[1]);
+                if(y!=pos[1])   printf("fuck Y\n");
+                if(z!=pos[2])   printf("fuck Z\n");
+                //printf("CHECK END\n");
+                // END CHECK
+                assert(currentBox >= 0);
+
 				if(boxSort->nbAtomToBox[currentBox] >0)
 				{
+                    //TODO optimisation par utilisation d'un tableau
 					for(int xadj = -1; xadj < 2; xadj++)
 						for(int yadj = -1; yadj < 2; yadj++)
 							for(int zadj = -1; zadj < 2; zadj++)
@@ -333,23 +415,26 @@ static void seq_force_cube (sotl_device_t *dev)
 								otherBox = get_num_box(dom,xother,yother,zother);
 								if(otherBox > -1 && boxSort->nbAtomToBox[otherBox] > 0)
 								{
-
+                                    computeForce(set, currentBox, otherBox);
+                                /*
 								  //  printf("current %d , other %d  \n", currentBox,otherBox);
 
-									for (unsigned current = boxSort->preNbAtomToBox[currentBox]; 
+                                    for (int current = boxSort->preNbAtomToBox[currentBox];
 									current < boxSort->preNbAtomToBox[currentBox+1]; 
 									current++) 
 									{
 									  
 										calc_t force[3] = { 0.0, 0.0, 0.0 };
 
-										for (unsigned other = boxSort->preNbAtomToBox[otherBox];
+                                        for (int other = boxSort->preNbAtomToBox[otherBox];
 										 other < boxSort->preNbAtomToBox[otherBox+1]; 
 										 other++)
 										{
 										  //  printf("coucou %d\n",other);
 											if (current != other ) //&& current != NULL && other != NULL)//current other pas négatif car unsigned 
 											{
+                                                assert(current >= 0);
+                                                assert(other >= 0);
 												sq_dist = squared_distance (set, current, other);
 
 												if (sq_dist < LENNARD_SQUARED_CUTOFF) 
@@ -368,12 +453,12 @@ static void seq_force_cube (sotl_device_t *dev)
 										set->speed.dx[current] += force[0];
 										set->speed.dx[set->offset + current] += force[1];
 										set->speed.dx[set->offset * 2 + current] += force[2];
-									}
+                                    }*/
 								}
 							}
 					}
 			}
-            //*/ // seq_force_old(dev);
+            //*/   seq_force_old(dev);
 	
 }
 
